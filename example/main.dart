10ff478444e3c6f87ba4340a1abf01d54c15341a
MRW Neundorf <ninevillage@gmail.com>
2022-01-23 03:33:31 +0100

    chore(lib): added example

---
+++ example/main.dart
@@ -0,0 +1,48 @@
+void main() async {
+  // Spree('url').authentication.createStorefrontUserToken(
+  //   'username',
+  //   'password',
+  // );
+
+  // Spree('url').authentication.createPlatformUserToken(
+  //   'username',
+  //   'password',
+  //   'client_id',
+  //   'client_secret',
+  //   'scope',
+  // );
+
+  // Spree('url').authentication.createPlatformAppToken(
+  //   'client_id',
+  //   'client_secret',
+  //   'scope',
+  // );
+
+  // Spree('url').authentication.refreshToken(
+  //   'token',
+  // );
+
+  // Spree('url').platform('token')...;
+
+  // Spree('url').storefront('token')...;
+
+  // use ENV VAR (default)
+  // SPREE_URL=
+  // SPREE_USERNAME=
+  // SPREE_PASSWORD=
+  // SPREE_CLIENT_ID=
+  // SPREE_CLIENT_SECRET=
+
+  // Spree().platform()...;
+  // Spree().storefront()...;
+
+  // use ENV VAR (custom prefix)
+  // SPREE_CUSTOM_URL=
+  // SPREE_CUSTOM_USERNAME=
+  // SPREE_CUSTOM_PASSWORD=
+  // SPREE_CUSTOM_CLIENT_ID=
+  // SPREE_CUSTOM_CLIENT_SECRET=
+
+  // Spree.prefix('SPREE_CUSTOM').platform()...;
+  // Spree.prefix('SPREE_CUSTOM').storefront()...;
+}
